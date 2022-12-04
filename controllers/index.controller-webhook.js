27d2c5c3ccc9c105ae066a2b2e57ec2c72317296
jean pierre <kreyesf@unamad.edu.pe>
2022-12-04 14:52:56 -0500

    api facebook-bot 6

--- controllers/index.controller-webhook.js
+++ controllers/index.controller-webhook.js
@@ -21,6 +21,7 @@ const postWebHook = (req, res) => {
     const body = req.body;
 
     if (body.object === 'page') {
+        console.log("si es una pagina");
         body.entry.forEach(entry => {
             //mensajes recibidos
             const webhookEvent = entry.messaging[0];
@@ -31,8 +32,10 @@ const postWebHook = (req, res) => {
 
             //valdiar el mensaje
             if (webhookEvent.message) {
+                console.log("validar 1");
                 handleMessage(sender_psid, webhookEvent.message);
             } else if (webhookEvent.postback) {
+                console.log("valdiar 2");
                 handlePostback(sender_psid, webhookEvent.postback);
             }
         });
@@ -59,12 +62,15 @@ const getWebHook = (req, res) => {
             res.status(200).send(challenge);
         } else {
             res.sendStatus(404);
+            console.log("error primario")
         }
     } else {
         res.sendStatus(404);
+        console.log("error secundario")
     }
 
 };
+
 
 function handleMessage(sender_psid, received_message) {
 
