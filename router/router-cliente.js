7c2d19dd173908f46a8772beea0f72d159fb8bf1
jean pierre <kreyesf@unamad.edu.pe>
2022-12-04 08:08:35 -0500

    subida 14 de vercel

--- router/router-cliente.js
+++ router/router-cliente.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const rutas = require('../controllers/index.controller-cliente');
 
-router.get('/', (req,res)=>{
-    res.send("ruta principal");
-});
-//router.get('/:id', rutas.secondCatalogo);
+router.get('/', rutas.mainCatalogo);
+router.get('/:id', rutas.secondCatalogo);
 module.exports = router;
