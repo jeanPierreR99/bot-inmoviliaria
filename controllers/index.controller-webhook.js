e61798ddb4ff6c5a58966b4af1c31774ea2a738f
jean pierre <kreyesf@unamad.edu.pe>
2022-12-04 15:13:11 -0500

    api facebook-bot 8

--- controllers/index.controller-webhook.js
+++ controllers/index.controller-webhook.js
@@ -21,7 +21,6 @@ const postWebHook = (req, res) => {
     const body = req.body;
 
     if (body.object === 'page') {
-        console.log("si es una pagina");
         body.entry.forEach(entry => {
             //mensajes recibidos
             const webhookEvent = entry.messaging[0];
@@ -32,10 +31,8 @@ const postWebHook = (req, res) => {
 
             //valdiar el mensaje
             if (webhookEvent.message) {
-                console.log("validar 1");
                 handleMessage(sender_psid, webhookEvent.message);
             } else if (webhookEvent.postback) {
-                console.log("valdiar 2");
                 handlePostback(sender_psid, webhookEvent.postback);
             }
         });
@@ -62,15 +59,12 @@ const getWebHook = (req, res) => {
             res.status(200).send(challenge);
         } else {
             res.sendStatus(404);
-            console.log("error primario")
         }
     } else {
         res.sendStatus(404);
-        console.log("error secundario")
     }
 
 };
-
 
 function handleMessage(sender_psid, received_message) {
 
@@ -78,15 +72,157 @@ function handleMessage(sender_psid, received_message) {
     let palabra = received_message.text;
     let palabraSplit = palabra.split(' ');
 
-    
-          
+    pool.query('select *from table_bot where input_bot = $1', [palabra], (err, customers) => {
+
+        let varResult;
+        let aux;
+        varResult = customers;
+
+        if (varResult.rowCount >= 1) {
+
+            aux = varResult.rows[0].output_bot;
+            response = {
+                'text': `${aux}`
+            }
+            callSendAPI(sender_psid, response);
+        }
+        else if (palabraSplit.length >= 2 && isNaN(palabraSplit[0])) {
+
+            let palabra1 = palabraSplit[0];
+            let palabra2 = palabraSplit[1];
+            let palabra3 = palabraSplit[2];
+
+            pool.query('select *from table_bot where input_bot = $1 or input_bot = $2 or input_bot = $3', [palabra1, palabra2, palabra3], (err, customers) => {
+
+                let varResult3;
+                let aux3;
+                varResult3 = customers;
+
+                if (varResult3.rowCount >= 1) {
+
+                    aux3 = varResult3.rows[0].output_bot;
                     response = {
-                        'text': `mensaje de respuesta`
+                        'text': `${aux3}`
                     }
-                    console.log("respuesta")
                     callSendAPI(sender_psid, response);
-        
- 
+                }
+                else {
+                    response = {
+                        'text': `Desconozco esa frase1`
+                    }
+                    callSendAPI(sender_psid, response);
+                }
+            })
+        }
+        else if (palabra.length == 8 && isFinite(palabra)) {
+
+            fetch("https://dniruc.apisperu.com/api/v1/dni/" + palabra + "?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imx1aXNwZmNhbmFsZXNAZ21haWwuY29tIn0.Vg8H-t3AU2FNwYPp8tBCVdjpuyl_7Q6CcLeL2Z3aR5k")
+                .then((res) => res.json())
+                .then(data => {
+                    if (data.message === 'No se encontraron resultadoss.') {
+                        response = {
+                            'text': `El dni no existe`
+                        }
+                        callSendAPI(sender_psid, response);
+                    }
+                    else {
+
+                        var nombres = data.nombres;
+                        var apellidos = data.apellidoPaterno + " " + data.apellidoMaterno;
+                        response = {
+                            'text': `Hola Sr(a). ${nombres}, ${apellidos} gracias por elegirnos ahora mandenos el 'ID' de la propiedad que esta interesada.
+ejemplo(cat-9999) tenga cuidado con las minusculas y mayusculas.`
+                        }
+                        callSendAPI(sender_psid, response);
+
+                    }
+                })
+        }
+        else {
+            pool.query('select *from table_catalogo where cat_loc_id = $1 or cat_hab_id = $1', [palabra], (err, customers) => {
+                
+                let varResult2;
+                let id, precio, direccion;
+                varResult2 = customers;
+
+                if (varResult2.rowCount >= 1 && varResult2.rows[0].cat_loc_estado == 'disponible') {
+
+                    id = varResult2.rows[0].cat_loc_id;
+                    precio = varResult2.rows[0].cat_loc_precio;
+                    direccion = varResult2.rows[0].cat_loc_dir;
+                    response = {
+                        'text': `la propiedad que usted a elegido es ${id} que tiene un precio de ${precio} y está ubicado en ${direccion}.
+Como ultimo paso mandenos un numero de contacto para confirmar su solicitud con su DNI y el ID de la propiedad que esta interesado ejemplo(944271227 74415423 cat-9999)`
+                    }
+                    callSendAPI(sender_psid, response);
+                }
+                else if (varResult2.rowCount >= 1 && varResult2.rows[0].cat_hab_estado == 'disponible') {
+
+                    id = varResult2.rows[0].cat_hab_id;
+                    precio = varResult2.rows[0].cat_hab_precio;
+                    direccion = varResult2.rows[0].cat_hab_dir;
+                    response = {
+                        'text': `la propiedad que usted a elegido es ${id} que tiene un precio de ${precio} y está ubicado en ${direccion}.
+Como ultimo paso mandenos un numero de contacto para confirmar su solicitud con su DNI y el ID de la propiedad que esta interesado ejemplo(944271227 74415423 cat-9999)`
+                    }
+                    callSendAPI(sender_psid, response);
+                }
+                else if (varResult2.rowCount >= 1 && varResult2.rows[0].cat_loc_estado != 'disponible' || varResult2.rowCount >= 1 && varResult2.rows[0].cat_hab_estado != 'disponible') {
+                   
+                    response = {
+                        'text': `la propiedad ${palabra} no esta disponible en estos momentos`
+                    }
+                    callSendAPI(sender_psid, response);
+                }
+                else if (palabraSplit.length == 3 && isFinite(palabraSplit[0]) && isFinite(palabraSplit[1]) && palabraSplit[1].length == 8 && isNaN(palabraSplit[2])) {
+                    fetch("https://dniruc.apisperu.com/api/v1/dni/" + palabraSplit[1] + "?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imx1aXNwZmNhbmFsZXNAZ21haWwuY29tIn0.Vg8H-t3AU2FNwYPp8tBCVdjpuyl_7Q6CcLeL2Z3aR5k")
+                    .then((res) => res.json())
+                    .then(data => {
+                        if (data.message === 'No se encontraron resultadoss.') {
+                            response = {
+                                'text': `El dni no existe, intentelo nuevamente`
+                            }
+                            callSendAPI(sender_psid, response);
+                        }
+                        else {
+                            pool.query('select *from table_catalogo where cat_hab_id = $1 or cat_loc_id = $1', [palabraSplit[2]], (err, customers) => {
+                               
+                                let varResult4;
+                                varResult4=customers;
+
+                                if (varResult4.rowCount >= 1){
+                                    var nombres = data.nombres;
+                                    var apellidos = data.apellidoPaterno + " " + data.apellidoMaterno;
+                                    var ac1= varResult4.rows[0].cat_loc_id || varResult4.rows[0].cat_hab_id;
+                                    console.log('nombres: '+nombres+' apellidos: '+apellidos+' ID: '+ac1);
+                                    //insetamos a la base de datos
+                                    //---------------------------------//
+                                    //fin de la insercion
+                                    response = {
+                                        'text': `Felicitaciones, sr(a). ${nombres}, ${apellidos} la propiedad ${ac1}
+ya se registro exitosamente en la base de datos nos pondremos en contacto con usted lo antes posible`
+                                    }
+                                    callSendAPI(sender_psid, response);
+                                }
+                                else{
+                                    response = {
+                                        'text': `el ID de la propiedad no existe, intentelo nuevamente`
+                                    }
+                                    callSendAPI(sender_psid, response);
+                                }
+                        })
+                    }
+                    })
+                }
+                else{
+                    response = {
+                        'text': `desconzoco esa frase 2`
+                    }
+                    callSendAPI(sender_psid, response);
+                }
+            });
+        }
+    });
 }
 
 function handlePostback(sender_psid, received_postback) {
