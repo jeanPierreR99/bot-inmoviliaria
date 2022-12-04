7221131e41dbbeea37589d67c781a58d18cadde1
jean pierre <kreyesf@unamad.edu.pe>
2022-12-04 10:16:08 -0500

    subida 29 de vercel

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const session = require('express-session');
 const app = express().use(bodyParser.json());
 app.use(express.urlencoded({extended:false}));
 
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
 
-app.use(express.json());
-app.use( express.static(path.join(__dirname, 'public')) );
 
 app.use(session({
     secret: "987f4bd6d4315c20b2ec70a46ae846d19d0ce563450c02c5b1bc71d5d580060b",
